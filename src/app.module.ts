96fd65c27fd5a26aa7f6d414bc80c517688a42ad
M <monica@nolatech.ai>
2024-02-29 18:19:17 -0400

    Connection to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CompanyModule } from './company/company.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [],
+  imports: [CompanyModule, MongooseModule.forRoot(process.env.MONGODB_URI)],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const pato: string = process.env.PATO;
+    return pato;
   }
 }
